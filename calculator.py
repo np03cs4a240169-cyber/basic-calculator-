e512e1ff14ee0704b0c0d1a8143f4145e2811611
np03cs4a240169-cyber <np03cs4a240169@heraldcollege.edu.np>
2026-03-30 12:08:27 +0545

    completed division

---
+++ calculator.py
@@ -0,0 +1,10 @@
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num2 != 0:
+    result = num1 / num2
+    print("Result:", result)
+else:
+    print("Error: Cannot divide by zero")
