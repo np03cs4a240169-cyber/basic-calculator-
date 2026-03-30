8571e468fa39229c8370518ac8a9e18850e5776f
Aakrist16 <aakristacharya47@gmail.com>
2026-03-30 11:42:51 +0545

    multiplication  completed

---
+++ calc.py
@@ -0,0 +1,8 @@
+print("This is for multiplication and you need to give 2 numbers")
+
+n1 = input("Enter the first number: ")
+n2 = input("Enter the second number: ")
+
+multi = n1*n2;
+
+print ("The multiple of "+  n1 +"and"+ n2 +"is" multi)
